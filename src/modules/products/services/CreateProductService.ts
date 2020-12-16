d46e7c02be5ff9c5e3d726b044ffed1aadf1507e
JoaoVSouto <joaovitorss99@hotmail.com>
2020-12-15 21:24:22 -0300

    Creates service for creating a new product

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const doesProductExist = await this.productsRepository.findByName(name);
+
+    if (doesProductExist) {
+      throw new AppError('There already exists a product with this name.');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
